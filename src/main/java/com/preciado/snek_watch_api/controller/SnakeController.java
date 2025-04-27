9c53dedb59e094bfd4a6cb7fc2bb34e01b04282b
Marco Preciado <mac46005@gmail.com>
2025-04-27 11:22:52 -0500

    Created a RestController and tested to make sure the app is working. It is working.

---
+++ src/main/java/com/preciado/snek_watch_api/controller/SnakeController.java
@@ -0,0 +1,19 @@
+package com.preciado.snek_watch_api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/snakes")
+public class SnakeController {
+    @GetMapping
+    public String getMethodName(
+        
+    ) {
+        return "Hello world!!!";
+    }
+    
+}
