96110beb1643fb0f66e952bff0cb6b8ac887462e
Marco Preciado <mac46005@gmail.com>
2025-04-28 08:58:01 -0500

    DietType enum has been added in the models folder.

---
+++ src/main/java/com/preciado/snek_watch_api/model/DietType.java
@@ -0,0 +1,7 @@
+package com.preciado.snek_watch_api.model;
+
+public enum DietType {
+    HERBIVORE,
+    CARNIVORE,
+    OMNIVORE
+}
---
+++ src/main/java/com/preciado/snek_watch_api/model/SnakeType.java
@@ -0,0 +1,16 @@
+package com.preciado.snek_watch_api.model;
+
+public class SnakeType {
+    private long id;
+    private String commonName;
+    private String taxonomyName;
+    private String overview;
+    private int averageLifeSpanInYrs;
+    private double averageAdultLength;
+    private DietType dietType;
+    private int minimumHabitatSizeInGallons;
+    private String[] supplies;
+
+
+
+}
