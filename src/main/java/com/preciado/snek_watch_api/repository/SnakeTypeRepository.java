974fa47e9fe719a303f504e8698945e4d8fb4bfe
Marco Preciado <mac46005@gmail.com>
2025-04-28 19:48:06 -0500

    Dependencies added for database connectivity. Will work on Repositories.

---
+++ src/main/java/com/preciado/snek_watch_api/repository/SnakeTypeRepository.java
@@ -0,0 +1,9 @@
+package com.preciado.snek_watch_api.repository;
+
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SnakeTypeRepository {
+    private final JdbcTemplateAutoConfiguration
+}
