62d281f9ab98313fa3e08ea298345ce727aecbb2
Marco Preciado <mac46005@gmail.com>
2025-04-29 08:50:09 -0500

    The create function of SnakeTypeRepository has been implemented. Needs testing though.

--- src/main/java/com/preciado/snek_watch_api/model/SnakeType.java
+++ src/main/java/com/preciado/snek_watch_api/model/SnakeType.java
@@ -9,4 +9,55 @@ public class SnakeType {
     private int averageLifeSpanInYrs;
     private double averageAdultLengthInFeet;
     private String diet;
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getCommonName() {
+        return commonName;
+    }
+    public void setCommonName(String commonName) {
+        this.commonName = commonName;
+    }
+    public String getTaxonomyName() {
+        return taxonomyName;
+    }
+    public void setTaxonomyName(String taxonomyName) {
+        this.taxonomyName = taxonomyName;
+    }
+    public String getCareLevel() {
+        return careLevel;
+    }
+    public void setCareLevel(String careLevel) {
+        this.careLevel = careLevel;
+    }
+    public String getOverview() {
+        return overview;
+    }
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+    public int getAverageLifeSpanInYrs() {
+        return averageLifeSpanInYrs;
+    }
+    public void setAverageLifeSpanInYrs(int averageLifeSpanInYrs) {
+        this.averageLifeSpanInYrs = averageLifeSpanInYrs;
+    }
+    public double getAverageAdultLengthInFeet() {
+        return averageAdultLengthInFeet;
+    }
+    public void setAverageAdultLengthInFeet(double averageAdultLengthInFeet) {
+        this.averageAdultLengthInFeet = averageAdultLengthInFeet;
+    }
+    public String getDiet() {
+        return diet;
+    }
+    public void setDiet(String diet) {
+        this.diet = diet;
+    }
+
+    
 }
--- src/main/java/com/preciado/snek_watch_api/repository/SnakeTypeRepository.java
+++ src/main/java/com/preciado/snek_watch_api/repository/SnakeTypeRepository.java
@@ -1,11 +1,19 @@
 package com.preciado.snek_watch_api.repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import com.preciado.snek_watch_api.model.SnakeType;
+
 @Repository
-public class SnakeTypeRepository {
+public class SnakeTypeRepository implements ICRUD<SnakeType> {
     private final JdbcTemplate jdbcTemplate;
 
     public SnakeTypeRepository(
@@ -14,6 +22,56 @@ public class SnakeTypeRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    // -- CREATE --
-    public void create()
+    @Override
+    public long create(SnakeType data) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        String sql = """
+            INSERT INTO snake_types (common_name, taxonomy_name, care_level, overview, average_life_span_in_yrs, average_adult_length_in_feet, diet) VALUES (?, ?, ?, ?, ?, ?, ?);
+        """;
+
+        jdbcTemplate.update(
+            connection -> {
+                PreparedStatement ps = connection.prepareStatement(
+                    sql,
+                    Statement.RETURN_GENERATED_KEYS
+                );
+
+                ps.setString(1, data.getCommonName());
+                ps.setString(2, data.getTaxonomyName());
+                ps.setString(3, data.getCareLevel());
+                ps.setString(4, data.getOverview());
+                ps.setInt(5, data.getAverageLifeSpanInYrs());
+                ps.setFloat(0, 0);
+                
+                return ps;
+            },
+            keyHolder
+        );
+        return keyHolder.getKey().longValue();
+    }
+
+    @Override
+    public List<SnakeType> read() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'read'");
+    }
+
+    @Override
+    public SnakeType read(long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'read'");
+    }
+
+    @Override
+    public boolean update(SnakeType data) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public boolean delete(SnakeType data) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
 }
